b160a2448e795162657f1a832cd052144db6722e
Failaliyev <Aliyev.fail@gmail.com>
2025-07-03 22:45:11 +0200

    Update Aufgabe4.java

--- src/Aufgabe4.java
+++ src/Aufgabe4.java
@@ -5,15 +5,15 @@ public class Aufgabe4 {
 
         Scanner scanner = new Scanner(System.in);
 
-        int zufallszahl = (int)(Math.random() * 5) + 1; // 1–5 arası təsadüfi ədəd seçilir
+        int zufallszahl = (int)(Math.random() * 5) + 1; 
         int versuch;
 
         System.out.println("Ich denke an eine Zahl zwischen 1 und 5. Rate sie!");
 
-        // do-while ilə istifadəçiyə bir neçə cəhd imkanı veririk
+ 
         do {
-            System.out.print("Dein Versuch: "); // təxmin istəyirik
-            versuch = scanner.nextInt();        // istifadəçi daxil edir
+            System.out.print("Dein Versuch: "); 
+            versuch = scanner.nextInt();        
 
             if (versuch != zufallszahl) {
                 System.out.println("Falsch, versuche es erneut!");
