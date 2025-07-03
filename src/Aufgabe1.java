da0a75699e5294074ce83fee9b0dd2323a8b7084
Failaliyev <Aliyev.fail@gmail.com>
2025-07-03 22:43:59 +0200

    Update Aufgabe1.java

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -1,5 +1,3 @@
-//Mövzu: for-each döngüsü
-//Məqsəd: Massivdə olan adları bir-bir çap etmək
 
 public class Aufgabe1 {
 
@@ -7,8 +5,6 @@ public class Aufgabe1 {
         String[ ] namen = {"Anna", "Fail", "Ilkin", "Lukas", "Ivan", "Mehmet"};
 
         System.out.println("Liste der Teilnehmer");
-
-        //Hər bir adı alıb çap edirik
 
         for (String name : namen) {
 
